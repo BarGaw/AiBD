0e79ffc01b8e5411b76811c81856e1f63eb18bd7
Bartłomiej <bartek.g700@gmail.com>
2021-10-12 01:43:07 +0200

    done

--- lab1.py
+++ lab1.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 my_func = lambda x: x ** 2 + 5
 v1 = np.linspace(-1, 1)
@@ -19,3 +20,12 @@ def plot_func(x, y, title='', x_ax='', y_ax=''):
 plot_func(v1, my_func(v1), '1', 'x axis', 'y axis')
 plot_func(v2, my_func(v2), '2', 'x axis', 'y axis')
 plot_func(v3, my_func(v3), '3', 'x axis', 'y axis')
+
+d = {'name': ['Ania', 'Basia', 'Celina', 'Daria', 'Ewa'],
+     'surname': ['A', 'B', 'C', 'D', 'E'],
+     'age': [1, 2, 3, 4, 5],
+     'sex': ['f', 'f', 'f', 'f', 'f']}
+df1 = pd.DataFrame(data=d)
+df1.info()
+df1.describe()
+print(df1.head(3))
